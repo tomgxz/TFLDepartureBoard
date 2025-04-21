42e93822876611327234d1f7aea31bbd50bb6ab1
Tom_gxz <75934344+tomgxz@users.noreply.github.com>
2025-04-21 18:00:09 +0100

    Working with GET requests to API

---
+++ site/static/font/README.md
@@ -0,0 +1,2 @@
+Source: [petykowski/London-Underground-Dot-Matrix-Typeface](https://github.com/petykowski/London-Underground-Dot-Matrix-Typeface)
+Licensed under [SIL Open Font License](http://scripts.sil.org/OFL)
--- site/static/js/index.js
+++ site/static/js/index.js
@@ -19,7 +19,8 @@ class UndergroundLine_Station {
         this.stopType = stopType;
         this.placeType = placeType;
         this.lines = lines;
-        this.platforms = platforms;
+        // this.platforms = platforms;
+        this.current_query_platforms = [];
         this.lat = lat;
         this.lon = lon;
         this.zone = zone;
@@ -27,25 +28,91 @@ class UndergroundLine_Station {
         this.loaded = true;
     }
 
-    async getTimetable(direction) {
-        if (!this.loaded) {
-            throw new Error("Station not loaded");
+    async getTimetable(line, platform_name) {
+        if (!this.loaded) {console.warn("Station not loaded"); return; }
+        if (!line.loaded) {console.warn("Line not loaded"); return;}
+
+        let response = await TFL_API_Handler.shared.get(`Line/${line.id}/Arrivals/${this.id}`);
+        let arrivals = [];
+
+        for (let arrival of response) {
+            if (arrival.platformName === platform_name) {
+                let destination = TFL_API_Handler.shared.undergroundStations[arrival.destinationNaptan];
+
+                arrivals.push(
+                    new UndergroundLine_Arrival(
+                        arrival.id,
+                        arrival,
+                        line,
+                        this,
+                        platform_name,
+                        arrival.currentLocation,
+                        destination,
+                        arrival.towards,
+                        arrival.timeToStation,
+                        arrival.expectedArrival
+                    )
+                );
+            }
         }
 
-        for (let line of this.lines) {
-            if (!line.loaded) {
-                throw new Error("Line not loaded");
-            }
+        arrivals.sort((a, b) => a.time_to_station - b.time_to_station);
 
-            let line_response = await TFL_API_Handler.shared.get(`Line/${line.id}/Arrivals/${this.id}`);
+        return arrivals;
+    }
 
-            for (let arrival of line_response) {
-                //console.log(`${arrival.lineName} Line: ${arrival.platformName} expected in ${arrival.timeToStation} seconds, heading towards ${arrival.towards}`);
+    async loadPlatforms(line) {
+        this.loaded = false;
+
+        let response = await TFL_API_Handler.shared.get(`Line/${line.id}/Arrivals/${this.id}`);
+        let platform_names = [];
+
+        for (let arrival of response) {
+            if (arrival.platformName !== null && !platform_names.includes(arrival.platformName)) {
+                platform_names.push(arrival.platformName);
             }
         }
+        
+        this.current_query_platforms = platform_names;
+        this.loaded = true;
     }
 }
 
+
+/**
+ * Represents an arrival of an underground train on a specific line.
+ * 
+ * @class
+ * @constructor
+ * @param {string} id - The unique identifier for the arrival.
+ * @param {Object} json_source - The JSON source object containing the arrival data, from the TFL API.
+ * @param {UndergroundLine} line - The underground line.
+ * @param {UndergroundLine_Station} station - The name of the station where the train is arriving.
+ * @param {string} platform_name - The platform where the train will arrive.
+ * @param {string} current_loc - Readable description of yhe current location of the train.
+ * @param {string} destination - The destination of the train.
+ * @param {string} towards - The terminus the train is heading towards.
+ * @param {number} time_to_station - The time (in seconds) until the train arrives at the station.
+ * @param {string|Date} expected - The expected arrival time of the train, as a string or Date object.
+ */
+class UndergroundLine_Arrival {
+    constructor(id, json_source, line, station, platform, current_loc, destination, towards, time_to_station, expected) {
+        this.id = id;
+        this.json_source = json_source;
+        this.line = line;
+        this.station = station;
+        this.platform = platform;
+        this.current_loc = current_loc;
+        this.destination = destination;
+        this.towards = towards;
+        this.time_to_station = time_to_station;
+        this.expected = new Date(expected);
+
+        this.loaded = true;
+    }
+}
+
+/*
 class UndergroundLine_Platform {
     constructor(id, json_source, name, number, line) {
         this.id = id;
@@ -57,6 +124,7 @@ class UndergroundLine_Platform {
         this.loaded = true;
     }
 }
+*/
 
 
 class TFL_API_Handler {
@@ -77,16 +145,35 @@ class TFL_API_Handler {
             content_type = "";
         }
 
-        const response = await fetch(
-            `${this.base_url}/${url}`, {
-                method: "GET",
-                headers: {
-                    "Cache-Control": "no-cache",
-                    "Content-Type": content_type,
-                    "app_key": this.app_key,
+        let response;
+        let retryAfter = 10;
+
+        while (true) {
+            response = await fetch(
+                `${this.base_url}/${url}`, {
+                    method: "GET",
+                    headers: {
+                        "Cache-Control": "no-cache",
+                        "Content-Type": content_type,
+                        "app_key": this.app_key,
+                    }
                 }
+            );
+
+            if (response.status === 429) {
+                if (retryAfter > 200) {
+                    console.error("Rate limit exceeded. Please try again later.");
+                    return null;
+                }
+
+                console.warn(`Rate limit exceeded. Retrying after ${retryAfter} seconds...`);
+
+                await new Promise(resolve => setTimeout(resolve, retryAfter * 1000));
+                retryAfter *= 2;
+            } else {
+                break;
             }
-        );
+        }
 
         if (type === "xml") {
             let text = await response.text();
@@ -105,7 +192,7 @@ class TFL_API_Handler {
         let underground_lines_json = await this.get(`Line/Mode/tube`);
 
         let stations_json = await this.get(`StopPoint/Type/NaptanMetroStation`);
-        let station_platforms_json = await this.get(`StopPoint/Type/NaptanMetroPlatform`);
+        // let station_platforms_json = await this.get(`StopPoint/Type/NaptanMetroPlatform`);
 
         underground_lines_json.forEach(async (line) => {
             let services = line.serviceTypes.map((serviceType) => serviceType.name);
@@ -123,6 +210,7 @@ class TFL_API_Handler {
                 }
             }
 
+            /*
             for (let platform_index in station_platforms_json) {
                 let platform  = station_platforms_json[platform_index];
 
@@ -137,7 +225,7 @@ class TFL_API_Handler {
                         new UndergroundLine_Platform(
                             platform.id,
                             platform,
-                            `Platform ${platform_number}`, // TODO
+                            `Platform ${platform_number}`,
                             platform_number,
                             null,
                         )
@@ -146,8 +234,7 @@ class TFL_API_Handler {
                     station_platforms_json.splice(platform_index, 1);
                 }
             }
-
-            console.log(platforms)
+            */
 
             this.undergroundStations[station.id] = new UndergroundLine_Station(
                 station.id,
@@ -172,8 +259,8 @@ class TFL_API_Handler {
             this.undergroundLines[line_id].loaded = true;
         }
 
-        this.loaded = true;
         update_select_options();
+        this.loaded = true;
     }
 }
 
@@ -213,7 +300,7 @@ const reset_select = (select, itemname, populate_items = null, filters = []) => 
 }
 
 
-const reset_select_platform = () => {
+const reset_select_platform = async () => {
     options_platform_select.empty();
 
     let text = "";
@@ -229,14 +316,21 @@ const reset_select_platform = () => {
             $(`<option />`).attr("value", "").text(text)
         );
     } else {
+        options_platform_select.append(
+            $(`<option />`).attr("value", "").text(`Loading platforms...`)
+        );
+
+        await selected_station.loadPlatforms(selected_line);
+
+        options_platform_select.empty();
 
         options_platform_select.append(
             $(`<option />`).attr("value", "").text(`Select a platform...`)
         );
         
-        for (let platform of selected_station.platforms) {
+        for (let platform of selected_station.current_query_platforms) {
             options_platform_select.append(
-                $(`<option />`).attr("value", platform.id).text(platform.name)
+                $(`<option />`).attr("value", platform).text(platform)
             );
         }
     }
@@ -254,14 +348,15 @@ const on_select_change_line = () => {
     let line_id = options_line_select.val();
 
     if (line_id === "") {
-        selected_line = null;
+        set_line(null);
+        update_board_text();
+
         reset_select(options_station_select, "station", TFL_API_Handler.shared.undergroundStations);
         reset_select_platform();
         return;
     }
 
-    selected_line = TFL_API_Handler.shared.undergroundLines[line_id];
-    selected_station = null;
+    set_line(TFL_API_Handler.shared.undergroundLines[line_id]);
     update_board_text();
 
     reset_select(
@@ -279,12 +374,14 @@ const on_select_change_station = () => {
     let station_id = options_station_select.val();
 
     if (station_id === "") {
-        selected_station = null;
+        set_station(null);
+        update_board_text();
+
         reset_select_platform();
         return;
     }
 
-    selected_station = TFL_API_Handler.shared.undergroundStations[station_id];
+    set_station(TFL_API_Handler.shared.undergroundStations[station_id]);
     update_board_text();
     
     reset_select_platform();
@@ -292,18 +389,49 @@ const on_select_change_station = () => {
 
 
 const on_select_change_platform = () => {
-    let platform_id = options_platform_select.val();
+    let platform = options_platform_select.val();
 
-    selected_platform = selected_station.platforms.find(platform => platform.id === platform_id);
+    if (platform === "") {
+        set_platform(null);
+        update_board_text();
 
-    if (platform_id === "" || selected_platform === undefined) {
-        selected_platform = null;
         return;
     }
 
+    set_platform(platform);
     update_board_text();
 }
 
+
+const set_line = line => {
+    selected_line = line;
+    set_url_param("line", line ? line.id : null);
+
+    selected_station = null;
+    set_url_param("station", null);
+
+    selected_platform = null;
+    set_url_param("platform", null);
+
+    changed_since_last_query = true;
+}
+
+const set_station = station => {
+    selected_station = station;
+    set_url_param("station", station ? station.id : null);
+
+    selected_platform = null;
+    set_url_param("platform", null);
+
+    changed_since_last_query = true;
+}
+
+const set_platform = platform => {
+    selected_platform = platform;
+    set_url_param("platform", platform);
+
+    changed_since_last_query = true;
+}
 
 // #endregion
 
@@ -317,28 +445,90 @@ const clear_board_text = () => {
     board_line_2_right.html("<br>");
 }
 
+
 const update_board_text = () => {
-    clear_board_text();
-    
-    board_line_1_left.text("London Underground Departures Board");
+    if (selected_line && selected_station && selected_platform) {
+        if (changed_since_last_query) {
+            changed_since_last_query = false;
 
-    if (!selected_line) {
-        board_line_2_left.text("Select a line and station...");
-        return;
-    } else if (!selected_station) {
-        board_line_2_left.text(`Select a station on the ${selected_line.name} line...`);
-        return;
-    } else if (!selected_platform) {
-        board_line_2_left.text("Select a platform...");
-        return;
+            if (arrival_query_timeout) {
+                clearTimeout(arrival_query_timeout);
+                arrival_query_timeout = null;
+            }
+
+            let query_fn = async () => {
+            let arrivals = await selected_station.getTimetable(selected_line, selected_platform);
+
+                if (arrivals.length === 0) {
+                    clear_board_text();
+                    board_line_1_left.text("No trains arriving");
+                }
+
+                const write = (arrival, index, left_node, right_node) => {
+                    if (!arrival) return;
+
+                    let tts_text = arrival.time_to_station > 60 ? `${Math.floor(arrival.time_to_station / 60)} min` : "Due";
+
+                    left_node.text(`${index}  ${arrival.towards}`);
+                    right_node.text(tts_text);
+                }
+
+                write(arrivals[0], 1, board_line_1_left, board_line_1_right);
+                write(arrivals[1], 2, board_line_2_left, board_line_2_right);
+
+                let next_query_time = Math.max(Math.min(
+                    arrivals[0]?.time_to_station % 30 || 30, 
+                    arrivals[1]?.time_to_station % 30 || 30, 
+                    30 // max of 30 seconds between requests
+                ), 5); // min of 5 seconds between requests
+
+                if ([NaN, undefined, null].includes(next_query_time)) {
+                    next_query_time = 30;
+                }
+
+                arrival_query_timeout = setTimeout(query_fn, next_query_time * 1000);
+            };
+
+            query_fn();
+        }
+
+    } else {
+        clear_board_text();
+    
+        board_line_1_left.text("London Underground Departures Board");
+    
+        if (!selected_line) {
+            board_line_2_left.text("Select a line and station...");
+            return;
+        } else if (!selected_station) {
+            board_line_2_left.text(`Select a station on the ${selected_line.name} line...`);
+            return;
+        } else if (!selected_platform) {
+            board_line_2_left.text("Select a platform...");
+            return;
+        }
     }
-    
-    clear_board_text();
-
-    board_line_1_left.text("Loading data...");
 }
 
 // #endregion
+
+
+const set_url_param = (key, value) => {
+    url_paramaters[key] = value;
+
+    let as_string = "?"
+
+    for (let [key, value] of Object.entries(url_paramaters)) {
+        if (value !== null && value !== "") {
+            if (as_string !== "?") {
+                as_string += "&";
+            }
+            as_string += `${key}=${value}`;
+        }   
+    }
+
+    window.history.replaceState({}, "", as_string);
+}
 
 
 const options_wrapper = $(".options-wrapper"),
@@ -356,7 +546,17 @@ const options_wrapper = $(".options-wrapper"),
       board_line_2_left = board_line_2.find("> span:nth-child(1)"),
       board_line_2_right = board_line_2.find("> span:nth-child(2)");
 
-let selected_line = null, selected_station = null, selected_platform = null;
+const url_paramaters = {};
+
+for (let [key, value] of new URLSearchParams(window.location.search).entries()) {
+    url_paramaters[key] = value
+}
+
+let selected_line = null, 
+    selected_station = null, 
+    selected_platform = null,
+    arrival_query_timeout = null,
+    changed_since_last_query = false;
 
 
 $(window).on("load", async () => {
@@ -378,10 +578,29 @@ $(window).on("load", async () => {
         await new Promise(resolve => setTimeout(resolve, 100));
     }
 
-    debug_station = TFL_API_Handler.shared.undergroundStations["940GZZLULNB"]; // london bridge
-    await debug_station.getTimetable("outbound")
+    let line_id = url_paramaters.line;
+    let station_id = url_paramaters.station;
+    let platform_id = url_paramaters.platform;
 
-    // stop_data = await TFL_API_Handler.shared.get("jp_public/api10/XML_STOPSTRUCTURE_REQUEST?sSStopNr=940GZZLUSJP", type="xml");
-    // stop_data = stop_data.getElementsByTagName("itdStopStructureRequest")[0];
-})
+    if (line_id && TFL_API_Handler.shared.undergroundLines[line_id]) {
+        selected_line = TFL_API_Handler.shared.undergroundLines[line_id];
+        options_line_select.val(line_id);
+        on_select_change_line();
 
+        if (station_id && TFL_API_Handler.shared.undergroundStations[station_id]) {
+            selected_station = TFL_API_Handler.shared.undergroundStations[station_id];
+            options_station_select.val(station_id);
+            on_select_change_station();
+
+            if (platform_id) {
+                await reset_select_platform();
+                
+                if (selected_station.current_query_platforms.includes(platform_id)) {
+                    selected_platform = platform_id;
+                    options_platform_select.val(platform_id);
+                    on_select_change_platform();
+                }
+            }
+        }
+    }
+});
